b74e9350e5ed1de9c4a57de7738f30ef99595fd7
monkmanmh <monkmanmh@yahoo.ca>
2021-10-26 09:10:35 -0700

    add package_load.R file

---
+++ package_load.R
@@ -0,0 +1,18 @@
+# load all of the packages required for 
+# _The Data Preparation Journey: Finding Your Way With R_
+
+# packages
+# tidyverse (extended)
+library(tidyverse)
+library(haven)
+
+# utilities
+library(fs)
+library(here)
+library(janitor)
+library(labelled)
+
+# data
+library(palmerpenguins)
+library(dpjr) # custom package for _The Data Preparation Journey_
+
