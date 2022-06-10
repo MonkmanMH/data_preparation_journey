ac09435f4b3a4019f6d9b8a32b28fe6ae47bf711
Martin Monkman <monkmanmh@yahoo.ca>
2022-06-10 07:07:15 -0700

    add package_install.R file

---
+++ package_install.R
@@ -0,0 +1,27 @@
+# install all of the packages required for 
+# _The Data Preparation Journey: Finding Your Way With R_
+# most recent update: 2022-06-10
+
+# packages
+# tidyverse (extended)
+install.packages(c(
+  "tidyverse",
+  "haven",
+  "readxl"
+  ))
+
+# utilities
+install.packages(c(
+  "fastDummies",
+  "fs",
+  "here",
+  "janitor",
+  "labelled"
+  ))
+
+# data
+install.packages("palmerpenguins")
+install.packages("dpjr") # custom package for _The Data Preparation Journey_
+
+
+
