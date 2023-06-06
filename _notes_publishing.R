84c1adad3a1034c0c5f518daff0c74f997860ab5
Martin Monkman <monkmanmh@yahoo.ca>
2023-06-05 22:00:42 -0700

    output

---
+++ _notes_publishing.R
@@ -0,0 +1,14 @@
+# code associated with publishing the book
+
+
+
+# local rendering
+
+bookdown::render_book()
+
+#
+bookdown::pdf_book()
+
+# publishing
+
+bookdown::publish_book(name = "DataPreparationJourney_draft", account = "martin_monkman")
